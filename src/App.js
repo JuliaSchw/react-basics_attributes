32aa0807c21f9a5f8d6f1abb8360db050cf9ee18
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-01 12:27:34 +0100

    challenge 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./styles.css";
 
+const myValue = "Hello World";
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Article Title</h2>
+      <label htmlFor="new-Article">Write something about Eurorack</label>
+      <input id="new-Article" value={myValue} />
+      <a className="article__link" href="https://intellijel.com/">
+        Link
+      </a>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
 }
